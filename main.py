40aeb5f5b2846bdafe693ef56bafd3b878e0ea32
unknown <diegowallace2010@gmail.com>
2021-06-27 17:18:04 -0300

    datos de variables
    variables

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
 import random
-
+j
 datos= {"fichas": 50,"probabilidad": 0.4, "maximo": 300}
+=======
+
+>>>>>>> Stashed changes
+def NDJ(datos):
+    fichas = datos["fichas"]
+    maximo = datos["maximo"]
+    p = datos["probabilidad"]
+    intentos = 0 
